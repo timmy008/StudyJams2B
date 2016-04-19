d94c4d52225a3cd0d66e7f955af9e9e7f0a95c81
timmy008 <474893384@qq.com>
2016-04-19 18:51:14 +0800

    sync

--- app/src/main/java/com/timmy008/studyjams2a/MainActivity.java
+++ app/src/main/java/com/timmy008/studyjams2a/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+
     }
 
     private void initView() {
